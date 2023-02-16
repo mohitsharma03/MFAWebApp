0c453654508a338b16c4334de4bfae52e45831e9
mohitsharma03 <03mohitsharma@gmail.com>
2023-02-16 19:04:43 +0530

    commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, Response
+import cv2
+import face_recognition
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            face_locations = face_recognition.face_locations(frame)
+            for (top, right, bottom, left) in face_locations:
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
