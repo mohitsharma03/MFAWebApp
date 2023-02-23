936870953e23aa46df7f57f482bc20809cee376d
sidk1023 <skolipara5@gmail.com>
2023-02-23 17:58:44 +0530

    Changes for client-server set-up

--- Code/app.py
+++ Code/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template,  request, Response, redirect
+from flask_cors import CORS
 import cv2
+import numpy as np
+import base64
+
 from pipeline import Pipeline 
 
 app = Flask(__name__)
+CORS(app)
 pipeLine = Pipeline()
 
 
@@ -15,6 +20,14 @@ def login():
 def test_cam():
     return render_template('webcam.html')
     
+@app.route('/sendImages', methods=['POST'])
+def image_process():
+    prev = data_uri_to_cv2_img(request.form["prev"])
+    curr = data_uri_to_cv2_img(request.form["curr"])
+    ret = pipeLine.pipeline2(prev,curr)
+    ret = base64.b64encode(ret)
+    return Response(ret)
+
 @app.route('/video_feed', methods=['POST'])
 def login_post():
     username = request.form['username']
@@ -37,5 +50,10 @@ def errorPg():
 #def video_feed():
 #   return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+def data_uri_to_cv2_img(uri):
+    encoded_data = uri.split(',')[1]
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    return img
 if __name__ == '__main__':
     app.run(debug=True)
--- Code/pipeline.py
+++ Code/pipeline.py
@@ -13,6 +13,86 @@ class Pipeline:
         self.diff = FrameDiff(0.92)
         self.detect = FaceDetect(0.50,"cpu")
         self.rec = FaceRecog(threshold = 0.2, metric = "cosine")
+        self.user = cv2.resize(cv2.imread("../Data/user.png"),(1000,1000))
+
+    def pipeline2(self,prev,curr):
+        prev_frame = cv2.resize(prev,(600, 400))
+        curr_frame = cv2.resize(curr,(600, 400))
+        frameCount = 2
+        noFaceCount = 0
+        multipleFaceCount = 0
+        unverifiedFaceCount = 0
+        spoofedFaceCount=0
+        outputStr = "{}, {}, {}\n"
+        #file = open("pipeline_output.txt","w")
+        start_time =time.time()
+        
+        try:
+            #frame_input_time = time.time()
+            diff_val, diff_score = self.diff.ssim(prev_frame, curr_frame)
+            if diff_val:
+                cv2.putText(curr_frame, 'Frame Change', (0,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                print("diff frame", diff_score)
+                numFaces, conf ,boxes = self.detect.detect(curr_frame)
+
+                if numFaces==1:
+                    print("1 face detected")
+                    (x,y,w,h) = boxes[0]
+                    x, y, w, h = int(x), int(y), int(w), int(h)
+                    text = f"{conf[0]*100:.2f}%"
+                    cv2.putText(curr_frame,text,(x, y - 20),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0, 255, 0),1)
+                    cv2.rectangle(curr_frame, (x, y), (w, h), (0, 255, 0), 1)
+                    isSame, distance = self.rec.verify(curr_frame,self.user)
+                    if(isSame):
+                        cv2.putText(curr_frame, 'Face Verified', (0,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                        print("face is verified", distance)
+                        #antispoof
+                        spoof_result = self.antiSpoof.predict_spoof(curr_frame)
+                        if spoof_result[1]>=0.9999:
+                            print("face is Fake", spoof_result[1])
+                            cv2.putText(curr_frame, 'Spoofed Face Detected', (0,45), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                            #file.write(outputStr.format("Spoofed Face Detected",frameCount,time.time()-start_time))
+                            spoofedFaceCount+=1
+
+                    else:
+                        cv2.putText(curr_frame, 'Face Not Verified', (0,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                        print("face is not verified", distance)
+                        #file.write(outputStr.format("Face Not Verified",frameCount,time.time()-start_time))
+                        unverifiedFaceCount+=1
+
+                    
+                elif numFaces==0:
+                    print("0 face detected")
+                    cv2.putText(curr_frame, 'No Faces', (0,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                    #file.write(outputStr.format("No Faces Detected",frameCount,time.time()-start_time))
+                    noFaceCount+=1
+                else:
+                    print("multiple faces detected")
+                    cv2.putText(curr_frame, 'Multiple Faces', (0,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+                    #file.write(outputStr.format("Multiple Faces Detected",frameCount,time.time()-start_time))
+                    multipleFaceCount+=1
+
+            else:
+                print("No difference")
+                cv2.putText(curr_frame, 'No Frame Change', (0,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+            #cv2.imshow("frame",curr_frame)
+            ret, buffer = cv2.imencode('.jpg', curr_frame)
+            frame = buffer.tobytes()
+            return frame
+            
+        except cv2.error:
+            print(cv2.error)
+        # if cv2.waitKey(25) & 0xFF == ord("q"):
+        #     break
+        # file.write("Summary\n")
+        # file.write("1. Missing Face: "+str(noFaceCount)+"\n")
+        # file.write("2. Multiple Faces in Frame: "+str(multipleFaceCount)+"\n")
+        # file.write("3. Unverified User: "+ str(unverifiedFaceCount)+"\n")
+        # file.write("4. Spoofed Face Count: "+ str(spoofedFaceCount)+"\n")
+        # file.close()
+    
+
+
         
     def runPipeline(self,video=0,source = "../Data/user.png"):
 
